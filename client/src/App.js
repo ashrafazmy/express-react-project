ddf0638e6c7f2f835f2adf1c4a736b5547a2b6c5
Ash <109843126+ashrafazmy@users.noreply.github.com>
2023-10-08 16:01:42 +0800

    add some api calls

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,63 @@ import React, {useEffect, useState} from 'react'
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}])
+  const [users, setUsers] = useState([]);
+  
+  useEffect(() => { 
+    getUsers()
+    }, [])
 
   async function getUsers(){
-    const response  = await fetch('/users');
-    const data = await response.json();
-    setCountries(data);
+    try {
+      const response = await fetch('/resume');
+      const data = await response.json();
+      console.log('data',data);
+      setUsers(data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  } 
 
-  }
-  useEffect(() => {
-    fetch("/users").then( //fetching the api
-      response => response.json() //getting the response in json
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
   return (
     <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        backendData.users.map((user, i) => (
-        <p key={i}>{user}</p>
-        ))
-      )}
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong>: {user.description}
+            <ul>
+              <h2>Projects</h2>
+
+              {user.projects.map((project) => (
+                <li key={project.id}>
+                  <strong>Project Name:</strong> {project.name}
+                </li>
+              ))}
+            </ul>
+
+            <ul>
+              <h2>Experiences</h2>
+
+              {user.experiences.map((exp) => (
+                <li key={exp.id}>
+                  <strong>Experience Title:</strong> {exp.title}
+                  <div>{exp.responsibilities}</div>
+                </li>
+              ))}
+            </ul>
+
+            <ul>
+              <h2>Education</h2>
+
+              {user.educations.map((edu) => (
+                <li key={edu.id}>
+                  <strong>School Name:</strong> {edu.school}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -10,24 +10,188 @@ const supabaseURL = process.env.SUPABASE_URL
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseURL, supabaseAnonKey);
+var data = [
+  {}
+]
 
-
-//get ALL users
-app.get('/users', async (req, res) => {
+app.get('/api', async (req, res) => {
   const {data, error} = await supabase
-      .from('users')
+      .from('routes')
+      .select()
+  res.send(data);
+});
+
+
+app.get('/api/projects', async (req, res) => {
+  const {data, error} = await supabase
+      .from('projects')
+      .select()
+  res.send(data);
+});
+
+app.get('/api/educations', async (req, res) => {
+  const {data, error} = await supabase
+      .from('educations')
+      .select()
+  res.send(data);
+});
+
+app.get('/api/experiences', async (req, res) => {
+  const {data, error} = await supabase
+      .from('experiences')
+      .select()
+  res.send(data);
+});
+
+app.get('/api/contacts', async (req, res) => {
+  const {data, error} = await supabase
+      .from('contacts')
       .select()
   res.send(data);
 });
 
 //get users by id
-app.get('/users/:id', async (req, res) => {
-  const {data, error} = await supabase
-      .from('users')
+app.get('/api/resume/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    //resume
+    const { data: user, error: userError } = await supabase
+      .from('resume')
       .select()
-      .is('id', req.params.id)
-  res.send(data);
+      .eq('id', userId);
+
+    if (userError) {
+      throw userError;
+    }
+
+    // projects
+    const { data: projects, error: projectsError } = await supabase
+      .from('projects')
+      .select()
+      .eq('resume_id', userId);
+
+    if (projectsError) {
+      throw projectsError;
+    }
+
+    // experiences
+    const { data: experiences, error: experienceError } = await supabase
+      .from('experiences')
+      .select()
+      .eq('resume_id', userId);
+
+    if (experienceError) {
+      throw experienceError;
+    }
+
+    //education
+    const { data: educations, error: educationError } = await supabase
+      .from('educations')
+      .select()
+      .eq('resume_id', userId);
+
+    if (educationError) {
+      throw educationError;
+    }
+
+    //contacts
+    const { data: contacts, error: contactError } = await supabase
+      .from('contacts')
+      .select()
+      .eq('resume_id', userId);
+
+    if (contactError) {
+      throw contactError;
+    }
+
+    // Combine the data into a single response object
+    const responseData = {
+      resume: {
+        ...user[0], // Assuming you expect only one user
+        projects,
+        experiences,
+        educations,
+        contacts
+      }
+    };
+
+    res.send(responseData);
+  } catch (error) {
+    res.status(500).send({ error: 'An internal server error occurred' });
+  }
 });
+
+app.get('/api/resume', async (req, res) => {
+  try {
+    const { data: users, error: usersError } = await supabase
+      .from('resume')
+      .select();
+
+    if (usersError) {
+      throw usersError;
+    }
+
+    const responseData = [];
+
+    for (const user of users) {
+      const userId = user.id;
+      // projects
+      const { data: projects, error: projectsError } = await supabase
+      .from('projects')
+      .select()
+      .eq('resume_id', userId);
+
+      if (projectsError) {
+        throw projectsError;
+      }
+
+      // experiences
+      const { data: experiences, error: experienceError } = await supabase
+        .from('experiences')
+        .select()
+        .eq('resume_id', userId);
+
+      if (experienceError) {
+        throw experienceError;
+      }
+      //education
+      const { data: educations, error: educationError } = await supabase
+        .from('educations')
+        .select()
+        .eq('resume_id', userId);
+
+      if (educationError) {
+        throw educationError;
+      }
+
+      //contacts
+      const { data: contacts, error: contactError } = await supabase
+        .from('contacts')
+        .select()
+        .eq('resume_id', userId);
+
+      if (contactError) {
+        throw contactError;
+      }
+
+      const userData = {
+        ...user, 
+        experiences,
+        projects,
+        educations,
+        contacts,
+      };
+
+      responseData.push(userData);
+    }
+
+    res.send(responseData);
+  } catch (error) {
+    res.status(500).send({ error: 'An internal server error occurred' });
+  }
+});
+
 
 
 app.get('/', (req, res) => {
