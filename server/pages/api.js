b9deb7991e7f184932d1dbf08b969eaa00165646
Ash <109843126+ashrafazmy@users.noreply.github.com>
2023-10-08 18:09:02 +0800

    test

--- server/pages/api.js
+++ server/pages/api.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       .select();
 
     if (usersError) {
-      throw usersError;
+      throw usersError; 
     }
 
     res.status(200).json(users);
